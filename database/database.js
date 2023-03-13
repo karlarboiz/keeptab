6e74a5e9c303cf029dda17a8475132815eae3ec2
Karl James <dickensallinone@gmail.com>
2023-03-13 10:36:29 +0800

    updated database for database connection

--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let mongodbURL = "mongodb+srv://karlarboiz:O2oaiaxkmIZj7huz@cluster0.ihvsaps.mongodb.net/?retryWrites=true&w=majority";
+
 if(process.env.MONGODB_URL) {
     mongodbURL = process.env.MONGODB_URL
 }
@@ -18,20 +19,14 @@ async function run(){
 
 function getDb() {
     if(!handleData) {
+        alert('Did not connect')
         throw {message: "Connection failed"}
     }
 
    return handleData
 }
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://karlarboiz:<password>@cluster0.ihvsaps.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
 
 module.exports = {
     runFunc: run,
