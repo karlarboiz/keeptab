70ac05be5f850906f457a76658d236ca41f9ccfb
Karl James <dickensallinone@gmail.com>
2023-03-13 15:10:25 +0800

    add something in the gitignore

--- database/database.js
+++ database/database.js
@@ -4,7 +4,6 @@ const MongoClient = mongodb.MongoClient;
 
 let mongodbURL = "mongodb+srv://karlarboiz:O2oaiaxkmIZj7huz@cluster0.ihvsaps.mongodb.net/?retryWrites=true&w=majority";
 
-
 let handleData;
 
 async function run(){
