19c32af8b9ee9d3acbf0973b2c46b92ce27e8f2c
Karl James <dickensallinone@gmail.com>
2023-03-13 15:55:03 +0800

    update .env file

--- database/database.js
+++ database/database.js
@@ -4,8 +4,8 @@ const MongoClient = mongodb.MongoClient;
 
 let mongodbURL = 'mongodb://localhost:27017'; 
 
-if(process.env.DB_MONGODBURL) {
-    mongodbURL = process.env.DB_MONGODBURL
+if(process.env.DB_USERNAME || process.env.DB_PASSWORD) {
+    mongodbURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ihvsaps.mongodb.net/?retryWrites=true&w=majority`
 }
 let handleData;
 
